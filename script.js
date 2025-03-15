8840c7183170b3a8042854b208a2e1b7556f2272
Yazan Alaa Yousef <86908063+yznalayosef@users.noreply.github.com>
2025-03-15 13:24:18 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function modifyContent() {
+    
+    document.getElementById('heading').textContent = "Task 3 continues";
+
+
+    document.getElementById('paragraph').classList.add('modified-paragraph');
+
+
+    document.getElementById('contentDiv').classList.add('modified-div');
+}
